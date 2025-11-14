320554775db730cbb32ba2ee19513541aad79a7e
Steve Clarke <steve@sevenview.ca>
2025-11-13 22:07:16 -0330

    feat: Add SimpleCov for code coverage and update .gitignore
    - Introduced SimpleCov for tracking test coverage, including a configuration file for custom settings and filters.
    - Updated Gemfile to include SimpleCov and its review formatter as dependencies.
    - Added coverage directory to .gitignore to prevent coverage reports from being tracked in version control.

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -2,6 +2,10 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+
+# See .simplecov for configuration
+require 'simplecov'
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
